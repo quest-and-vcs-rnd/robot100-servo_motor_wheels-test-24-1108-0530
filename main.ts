5db67848a93a6b490f8a156b73782714bfe14457
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-11-08 05:26:09 -0800

    24-11089-0530 First Draft

--- main.ts
+++ main.ts
@@ -1,6 +1,87 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        . # . . .
+        . # . . .
+        . # . # .
+        `)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    50,
+    0
+    )
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(0, quest_Time_Units_Enum.Seconds)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . # .
+        `)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    0,
+    0
+    )
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        . # . # .
+        . # . # .
+        . # . # .
+        `)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    50,
+    50
+    )
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(0, quest_Time_Units_Enum.Seconds)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . # .
+        `)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    0,
+    0
+    )
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . . # .
+        . . . # .
+        . # . # .
+        `)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    0,
+    50
+    )
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(0, quest_Time_Units_Enum.Seconds)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . # .
+        `)
+    quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_Func(
+    quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+    0,
+    0
+    )
+})
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"2-Sequence Animation Validates New Start"
+)
 basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
-basic.forever(function () {
-	
-})
