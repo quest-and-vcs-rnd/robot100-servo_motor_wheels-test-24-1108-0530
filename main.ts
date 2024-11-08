e031bca9049507c1ea3bab8719ebbaa191784137
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-11-08 05:24:31 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.showIcon(IconNames.Heart)
+quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+	
+})
