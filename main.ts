ede1b59d46c76d580c8918c7db849c6b41d92ec1
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-11-08 05:40:17 -0800

    24-1108-0540 Copyright and Version-Id

--- main.ts
+++ main.ts
@@ -85,3 +85,9 @@ quest_Note_1.quest_Show_String_For_Note_Small_Func(
 basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"©️ 2024 Quest Institute. All rights reserved."
+)
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"Ver 2.1 (0.0.2, 24-1108-0540)"
+)
