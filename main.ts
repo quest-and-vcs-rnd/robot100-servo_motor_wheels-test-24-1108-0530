d107ec47f063eb9f657f0bce84348f011851c9cc
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-12-17 06:22:32 -0800

    robot100-servo_motors-test-v2.1 (24-1217-0630)

--- main.ts
+++ main.ts
@@ -92,9 +92,9 @@ basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
 Setup_Function()
-quest_Note_1.quest_Show_String_For_Note_Small_Func(
+quest_Note_1.quest_Show_String_For_Note_Big_Func(
 "©️ 2024 Quest Institute. All rights reserved."
 )
-quest_Note_1.quest_Show_String_For_Note_Small_Func(
-"Ver 2.1 (0.0.5, 24-1108-0550)"
+quest_Note_2.quest_Show_String_For_Note_Big_Func(
+"Can Start Your Code Below"
 )
