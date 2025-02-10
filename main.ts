d12fd7d023828aabf5eceaf70cd7f6eb466dcb9d
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-02-10 13:15:11 -0800

    25-0210-1300 *** https://github.com/jasonc1025-333/23-1006-1820-rq100-questcustomblocks-wukong-oled.git:v0.14.2 *** Add 'manual_servo_motors w/timer' & 'auto_turn w/ timer'

--- main.ts
+++ main.ts
@@ -88,6 +88,112 @@ input.onButtonPressed(Button.B, function () {
     0
     )
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (true) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        50,
+        50,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        -50,
+        -50,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        50,
+        -50,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        -50,
+        50,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.right,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+    if (true) {
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            . . . . .
+            . . . . .
+            `)
+        quest_Motors.quest_Set_Turn_WithTimer_Func(
+        quest_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
+        quest_Turn_Type_Enum.Pivot,
+        quest_Turn_Direction_Enum.left,
+        quest_Turn_Power_Enum.Lo,
+        quest_Turn_Duration_Enum.msec_3000,
+        quest_Debug_Show_Enum.Off
+        )
+        quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(3, quest_Time_Units_Enum.Seconds)
+    }
+})
 function pre_setup_System_AnyLessonAlways_Func () {
     quest_Note_1.quest_Show_String_For_Note_Small_Func(
     "Fix 'show leds' Block's Orientation for Driving"
