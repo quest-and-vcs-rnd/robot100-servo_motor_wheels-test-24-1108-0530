4a9dfe0ee63f1c054e851b6f252336741b0706a3
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-02-10 12:43:02 -0800

    25-0210-1250 *** Add 4 Setup Functions

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+function setup_Staff_ThisLessonOnly_Func () {
+	
+}
+function setup_System_AnyLessonAlways_Func () {
+	
+}
+function setup_Student_ThisLessonOnly_Func () {
+	
+}
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . . # .
@@ -79,22 +88,25 @@ input.onButtonPressed(Button.B, function () {
     0
     )
 })
-function Setup_Function () {
+function pre_setup_System_AnyLessonAlways_Func () {
     quest_Note_1.quest_Show_String_For_Note_Small_Func(
     "Fix 'show leds' Block's Orientation for Driving"
     )
     display.rotateTo(display.Direction.UpsideDown)
 }
+pre_setup_System_AnyLessonAlways_Func()
 quest_Note_1.quest_Show_String_For_Note_Small_Func(
 "2-Sequence Animation Validates New-Start"
 )
 basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
-Setup_Function()
+quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+setup_Staff_ThisLessonOnly_Func()
+setup_System_AnyLessonAlways_Func()
 quest_Note_1.quest_Show_String_For_Note_Big_Func(
-"©️ 2024 Quest Institute. All rights reserved."
+"©️ 2025 Quest Institute. All rights reserved."
 )
 quest_Note_2.quest_Show_String_For_Note_Big_Func(
-"Can Start Your Code Below"
+"Student Setup-Code Can Start Below.."
 )
